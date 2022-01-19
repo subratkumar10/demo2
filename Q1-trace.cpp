973f372ad27c85c0a238d13a76997c9a349b7a5c
subrat kumar pradhan <kumarsubratpradhan.10@gmail.com>
2022-01-20 00:27:08 +0530

    init

---
+++ Q1-trace.cpp
@@ -0,0 +1,71 @@
+
+#include<iostream>
+using namespace std;
+int find_max_element(int a[],int n)
+{
+  int result;
+   static int activation_record_id=1;
+    cout<<"///***Calling find_max_element function(a,"<<n<<"):Activation record "<<activation_record_id<<endl;
+    cout<<"Variable            \tvalue  \taddress\n";
+    cout<<"n                   \t"<<n<<"\t"<<"   "<<&n<<endl;
+    cout<<"activation_record_id\t"<<activation_record_id<<"    \t"<<&activation_record_id<<endl;
+  if((n==1))
+   {
+      result=a[0];
+       cout<<"*********Returning from find_max_element(a,"<<n<<")"<<": Activation Record"<<activation_record_id<<endl;
+   cout<<"Variable            \tAddress\t\tValue\n";
+   cout<<"n                   \t"<<&n<<"\t"<<n<<endl;
+   cout<<" result                 \t"<<&result<<"\t"<<result<<endl;
+   cout<<"activation_record_id\t"<<&activation_record_id<<"\t"<<activation_record_id<<endl;
+      return result;
+   }
+  else
+   {
+
+      activation_record_id++;
+
+      result=max(a[n-1],find_max_element(a,n-1));
+      activation_record_id--;
+      cout<<"*********Returning from find_max_element(a,"<<n<<")"<<": Activation Record"<<activation_record_id<<endl;
+   cout<<"Variable            \tAddress\t\tValue\n";
+   cout<<"n                   \t"<<&n<<"\t"<<n<<endl;
+   cout<<" result                 \t"<<&result<<"\t"<<result<<endl;
+   cout<<"activation_record_id\t"<<&activation_record_id<<"\t"<<activation_record_id<<endl;
+      return result;
+   }
+}
+int main()
+{
+ int n,f;
+ cout<<"enter the sizeof the array"<<endl;
+ cin>>n;
+ int a[n];
+ cout<<"enter the elements of the array"<<endl;
+ for(int i=0;i<n;i++)
+ {
+     cin>>a[i];
+ }
+ static int activation_record_id=1;
+
+
+   cout<<"***** trace of getting maximum  elements of the array "<<endl;
+   cout<<"*********called Main function, main(): Activation Record"<<activation_record_id<<endl;
+   cout<<"Variable            \tAddress\t\tValue\n";
+   cout<<"n                   \t"<<&n<<"\t"<<n<<endl;
+   cout<<"f                   \t"<<&f<<"\t"<<f<<endl;
+   cout<<"activation_record_id\t"<<&activation_record_id<<"\t"<<activation_record_id<<endl;
+
+
+   f=find_max_element(a,n);
+   cout<<"*********Returning from Main function, main(): Activation Record"<<activation_record_id<<endl;
+   cout<<"Variable            \tAddress\t\tValue\n";
+   cout<<"n                   \t"<<&n<<"\t"<<n<<endl;
+   cout<<"f                   \t"<<&f<<"\t"<<f<<endl;
+   cout<<"activation_record_id\t"<<&activation_record_id<<"\t"<<activation_record_id<<endl;
+   cout<<"Final result:the maximum element is  "<<f<<endl;
+
+
+
+ return 0;
+}
+
---
+++ Q2-trace.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+
+using namespace std;
+void print(int a[],int end1)
+{
+    for(int i=0;i<=end1;i++)
+    {
+        cout<<a[i]<<" ";
+    }
+}
+void permutation(int a[],int start,int end1){
+    static int activation_record=1;
+    cout<<"************Calling permutation function-->permutation(arr[";
+   print(a,end1);
+    cout<<"]"<<",("<<start<<","<<end1<<")): Activation record "<<activation_record<<".\n";
+    cout<<"Variable            \tvalue  \taddress\n";
+    cout<<"start                   \t"<<start<<"   \t"<<&start<<endl;
+    cout<<"end1                   \t"<<end1<<"   \t"<<&end1<<endl;
+    cout<<"activation_record\t"<<activation_record<<"   \t"<<&activation_record<<endl;
+    if(start==end1){
+        cout<<"***********Returning from permutation function--->permutation(arr[";
+        print(a,end1);
+        cout<<"]"<<",("<<start<<","<<end1<<")): Activation record "<<activation_record<<".\n";
+        cout<<"Variable            \tvalue  \taddress\n";
+        cout<<" start                  \t"<<start<<"   \t"<<&start<<endl;
+        cout<<" end1                  \t"<<end1<<"   \t"<<&end1<<endl;
+        cout<<"activation_record\t"<<activation_record <<"   \t"<<&activation_record<<endl;
+        cout<<"the permutation of the array is ";print(a,end1);cout<<endl;
+
+        return;
+    }
+    for(int i=start;i<=end1;i++){
+        swap(a[start],a[i]);
+        activation_record++;
+        permutation(a,start+1,end1);
+        activation_record--;
+        swap(a[start],a[i]);
+    }
+    cout<<"*********Returning from the permutation function---->permutation(arr[";
+    print(a,end1);
+    cout<<"]"<<",("<<start<<","<<end1<<"))--> activation record "<<activation_record<<endl;
+    cout<<"Variable            \tvalue  \taddress\n";
+    cout<<" start                  \t"<<start<<"   \t"<<&start<<endl;
+    cout<<"  end1                   \t"<<end1<<"   \t"<<&end1<<endl;
+    cout<<"activation_record\t"<<activation_record<<"   \t"<<&activation_record<<endl;
+    return;
+}
+int main()
+{
+    static int activation_record;
+    int n;
+    cout<<"Enter the size of the array";
+    cin>>n;
+    int a[n];
+    int i=0;
+    while(i<n)
+    {
+      a[i]=i+1;
+      i++;
+    }
+
+   cout<<"***** trace of getting maximum  elements of the array "<<endl;
+   cout<<"*********called Main function, main(): Activation Record"<<activation_record<<endl;
+   cout<<"Variable            \tAddress\t\tValue\n";
+   cout<<"n                   \t"<<&n<<"\t"<<n<<endl;
+   cout<<"activation_record\t"<<&activation_record<<"\t"<<activation_record<<endl;
+    permutation(a,0,n-1);
+    cout<<"***************Returning from main function: Activation record*************** "<<activation_record<<".\n";
+    cout<<"Variable            \tvalue  \taddress"<<endl;
+    cout<<"n                   \t"<<n<<"   \t"<<&n<<endl;
+    cout<<"activation_record\t"<<activation_record<<"   \t"<<&activation_record<<endl;
+    return 0;
+
+}
